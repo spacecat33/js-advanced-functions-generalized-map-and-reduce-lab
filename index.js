f6abb04bdd542d61aaefa16cdaabb3920ad56005
Janan Akkad <jananakkad@hotmail.co.uk>
2023-03-22 15:08:13 +0000

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+
+function map(array, fn) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        let result = fn(array[i])
+        newArray.push(result)
+    };
+    return newArray;
+};
+
+function reduce(array, fn, startingPoint) {
+    let newValue = (!!startingPoint) ? startingPoint : array[0];
+    let i = (!!startingPoint) ? 0 : 1
+    for (; i < array.length; i++) {
+        newValue = fn(array[i], newValue)
+    };
+    return newValue;
+}
